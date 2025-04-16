b1586e30e22840d40174929b1c97a79ecd719610
Mritunjay Patel <97620757+Mrpatel2319@users.noreply.github.com>
2025-04-16 14:45:45 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import 'regenerator-runtime/runtime'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
